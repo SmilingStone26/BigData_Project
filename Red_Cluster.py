23d1f71912d6cc661f776aac3d987bf0840d6f7f
Sankukai <smilingstone26@gmail.com>
2021-12-02 21:29:53 +0100

    Finalisation du Projet

--- Red_Cluster.py
+++ Red_Cluster.py
@@ -9,8 +9,7 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import StandardScaler
 
-axe_X = 7
-axe_Y = 10
+
 liste_type = ["Type", "Fixed acidity", "Volatile acidity", "Citric acid", "Residual sugar", "Chlorides", "Free sulfur dioxyde", "Total sulfur dioxide", "Density", "pH", "Sulphates", "Alcohol", "Quality"]
 csvFile = open("Resource/winequality-red.csv", encoding='utf-8')
 readFile = list(csv.reader(csvFile, delimiter = ";"))
@@ -46,6 +45,7 @@ for i in range(1,len(readFile)):
     red_Wine.append([fixed_acidity[i-1],volatile_acidity[i-1],citric_acid[i-1],residual_sugar[i-1],chloride[i-1],free_sulfure_dioxyde[i-1],total_sulfur_dioxyde[i-1],density[i-1],pH[i-1],sulphates[i-1],alcohol[i-1],quality[i-1]])
 csvFile.close()
 
+##----------------------------------- Test divers ----------------------------------------------------##
 #------------------------------------Clusterisation 1 --------------------------------------------------
 """
 k_means_Red = cluster.KMeans(n_clusters=6,random_state=0)
@@ -235,23 +235,60 @@ plt.scatter(quality,label_Red)
 plt.subplot(212)
 plt.hist2d(quality,label_Red, bins=3)
 plt.show()'''
+##---------------------- Fin des test ------------------------##
+
+##--------------------- Résultats gardés --------------------##
 
 #---------------------- cluster 6 -----------------------------
 
 red_Wine_6 = []
 
 for i in range(len(red_Wine)):
-  # red_Wine_6.append([fixed_acidity[i] / 15.9, volatile_acidity[i]/1.58, citric_acid[i], residual_sugar[i]/15.5, chloride[i], free_sulfure_dioxyde[i]/72, total_sulfur_dioxyde[i]/289, density[i], pH[i]/4, sulphates[i]/2, alcohol[i]/14.9])
-  red_Wine_6.append(
+    red_Wine_6.append(
       [fixed_acidity[i] , volatile_acidity[i] , citric_acid[i], residual_sugar[i] , chloride[i],
        free_sulfure_dioxyde[i] , total_sulfur_dioxyde[i] , density[i], pH[i] , sulphates[i] ,
        alcohol[i] ])
 
-#ph et alcool ou alcool et acid citrique : cluster en 0 - 0.5
-#ph, alcool et acid citrique : cluster en 2.5-3
 
-  # red_Wine_6.append([fixed_acidity[i]])
 
+#----------------- Calcul du Kmeans avec 6 clusters ----------------
+k_means_Red = cluster.KMeans(n_clusters=6,random_state=0)
+k_means_Red.fit(red_Wine_6)
+label_Red = k_means_Red.labels_
+
+Red_Cluster_0 = []
+Red_Cluster_1 = []
+Red_Cluster_2 = []
+Red_Cluster_3 = []
+Red_Cluster_4 = []
+Red_Cluster_5 = []
+
+for i in range(len(red_Wine_6)):
+    if(label_Red[i] == 0):
+        Red_Cluster_0.append(red_Wine_6[i])
+    elif (label_Red[i] == 1):
+        Red_Cluster_1.append(red_Wine_6[i])
+    elif (label_Red[i] == 2):
+        Red_Cluster_2.append(red_Wine_6[i])
+    elif (label_Red[i] == 3):
+        Red_Cluster_3.append(red_Wine_6[i])
+    elif (label_Red[i] == 4):
+        Red_Cluster_4.append(red_Wine_6[i])
+    elif (label_Red[i] == 5):
+        Red_Cluster_5.append(red_Wine_6[i])
+
+plt.figure(1)
+plt.subplot(211)
+plt.title("Kmeans avec 6 clusters")
+plt.scatter(quality,label_Red)
+plt.subplot(212)
+plt.hist2d(quality,label_Red, bins=[6,6])
+plt.show()
+
+#---------------------------------------------------------------
+
+#----------------- Calcul des silhouette ----------------------------
+print("Calcul de la silhouette")
 for k in range(2,11) :
   k_means_Red = cluster.KMeans(n_clusters=k,random_state=0)
   k_means_Red.fit(red_Wine_6)
@@ -259,11 +296,13 @@ for k in range(2,11) :
 
   silhouette_avg = silhouette_score(red_Wine_6, label_Red)
   print(
-      "Pour k = ", k,
-    "The average silhouette_score is :",
+      "For k = ", k,
+    "The average silhouette_score of red wine is :",
     silhouette_avg,
   )
+#-------------------------------------------------------------------
 
+#----------------- Calcul du Kmeans avec 2 clusters ----------------
 k_means_Red = cluster.KMeans(n_clusters=2,random_state=0)
 k_means_Red.fit(red_Wine_6)
 label_Red = k_means_Red.labels_
@@ -291,6 +330,7 @@ for i in range(len(red_Wine_6)):
 
 plt.figure(1)
 plt.subplot(211)
+plt.title("Kmeans avec 2 clusters")
 plt.scatter(quality,label_Red)
 plt.subplot(212)
 plt.hist2d(quality,label_Red, bins=[2,2])
